d7ff19af2d80e53eee59c78bb46c1d566e695ad2
FatwaArya <fatwaaryasatyaakbar@gmail.com>
2022-03-13 21:28:12 +0700

    monster rolodex

---
+++ react-basics/src/App.js
@@ -0,0 +1,49 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react';
+import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      monsters: [],
+      searchField: ''
+    }
+  }
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((res) => res.json())
+      .then((users) => this.setState(() => {
+        return { monsters: users }
+      }))
+
+  }
+  onSearchChange = (event) => {
+
+    const searchField = event.target.value.toLocaleLowerCase()
+    this.setState(() => {
+      return { searchField }
+    })
+  }
+  render() {
+    const { monsters, searchField } = this.state
+    const { onSearchChange } = this
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField)
+    })
+
+    return (
+      <div className="App">
+        <h1 className='app-title'>Monster Rolodex</h1>
+        <SearchBox className='monster-search-box' onChangeHandler={onSearchChange} placeholder='search monster' />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
+
+
+}
+
+export default App;
